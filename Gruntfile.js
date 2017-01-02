e62a079f3ae131d6af7139c2b260e9bcfced8c14
Ian Claudius <ianclaudius@me.com>
2017-01-02 12:36:43 -0600

    Image optimizations
    Created resized/optimized thumbnail versions. Pulled imagemin from
    grunt file in favor of executing additional optimizations locally

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,6 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 	});
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-	grunt.registerTask('default', ['imagemin']);
+	grunt.registerTask('default', []);
 };
-
-
-imagemin: {
-  dynamic: {
-    files: [{
-      expand: true,
-      cwd: 'img/',
-      src: ['**/*.{png,jpg,gif}'],
-    }]
-  }
-}
