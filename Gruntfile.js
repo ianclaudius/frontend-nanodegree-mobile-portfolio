5e711e995edf8eb7c3d1b4824d457ae87b8e608a
Ian Claudius <ianclaudius@me.com>
2017-01-02 11:57:46 -0600

    Image optimization
    No resizing yet, just optimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,18 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 	});
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-	grunt.registerTask('default', []);
+	grunt.registerTask('default', ['imagemin']);
 };
+
+
+imagemin: {
+  dynamic: {
+    files: [{
+      expand: true,
+      cwd: 'img/',
+      src: ['**/*.{png,jpg,gif}'],
+    }]
+  }
+}
